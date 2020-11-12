76c5d4e85858ef136d1e16a186477db90f2c3276
Khaildoran <sgermscheid@gmail.com>
2020-11-12 13:37:07 -0600

    created add item form

---
+++ src/redux/sagas/addItem.saga.js
@@ -0,0 +1,20 @@
+import axios from 'axios';
+import { put, takeEvery } from 'redux-saga/effects';
+
+//send axios request to shelf.router to get all items from shelf DB
+function* addItemSaga(action) {
+  try{
+    yield axios.post('/api/shelf', action.payload)
+    yield put({
+        type: "FETCH_SHELF"
+    })
+  } catch(error){
+    console.log('Error in addItem SAGA',error);
+  }
+}
+
+function* addItem() {
+    yield takeEvery('ADD_SHELF_ITEM', addItemSaga);
+  }
+
+export default addItem;
